b81effe6563433d30d3f9ae976b33e961319045f
thilankadilshan <thilanka.cv@gmail.com>
2025-04-12 20:47:46 +0530

    Navbar design
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch thilanka1
    Your branch is up to date with 'origin/thilanka1'.
    
    Changes to be committed:
    	modified:   client/src/components/Header/Header.css
    	modified:   client/src/components/Header/Header.jsx
    	modified:   client/src/components/NavBar/NavBar.css
    	modified:   client/src/components/SearchBar/SearchBar.css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Header.css';
-import Logo from '../../assets/logo.png'; // Update with actual logo path
+import Logo from '../../assets/logo.png';
 import SearchBar from '../SearchBar/SearchBar';
 import NavBar from '../NavBar/NavBar';
 
 const Header = () => {
     return (
-        <div className="header">
-            <img src={Logo} alt="TractorPartsLk" className="logo" />
-            <h1 className="title">TractorPartsLk</h1>
+        <header className="header">
+            <div className="branding">
+                <img src={Logo} alt="TractorPartsLk" className="logo" />
+                <h1 className="title">TractorPartsLk</h1>
+            </div>
             <SearchBar />
             <NavBar />
-        </div>
+        </header>
     );
 };
 
