7aa4a9f83e0ae1fda24b867b517d18d28397bbed
thilankadilshan <thilanka.cv@gmail.com>
2025-04-13 00:27:44 +0530

    hero section 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.js 	new file:   client/src/assets/hero1.png 	new file:   client/src/assets/hero2.png 	new file:   client/src/assets/hero3.png 	new file:   client/src/assets/hero4.png 	new file:   client/src/assets/hero5.png 	new file:   client/src/components/HeroSection/HeroSection.css 	new file:   client/src/components/HeroSection/HeroSection.jsx 	new file:   client/src/pages/Buyer/HomePage/HomePage.css 	new file:   client/src/pages/Buyer/HomePage/HomePage.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import HeroSection from './components/HeroSection/HeroSection';
 import './App.css';
+
 
 
 const App = () => {
@@ -8,6 +10,7 @@ const App = () => {
         <div>
             <Header />
             {/* Additional components can be added here */}
+            <HeroSection />
         </div>
     );
 };
