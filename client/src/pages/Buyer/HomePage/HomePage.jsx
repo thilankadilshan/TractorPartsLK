af9d9d509f69f5836d41ca1be05996ccdc9b625c
thilankadilshan <thilanka.cv@gmail.com>
2025-04-19 15:30:46 +0530

    Changes to be committed: 	new file:   client/src/pages/Brands/JohnDeerePage.jsx 	new file:   client/src/pages/Brands/MahindraPage.jsx 	new file:   client/src/pages/Brands/SonalikaPage.jsx 	new file:   client/src/pages/Brands/TafePage.jsx 	modified:   client/src/pages/Buyer/HomePage/HomePage.jsx

--- client/src/pages/Buyer/HomePage/HomePage.jsx
+++ client/src/pages/Buyer/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../../../components/HeroSection/HeroSection";
 import Header from "../../../components/Header/Header";
+import TractorBrands from "../../../components/TractorBrands/TractorBrands";
 
 const Dashboard = () => {
   return (
     <div>
-      <Header/>
-      <HeroSection/>
+      <Header />
+      <HeroSection />
+      <TractorBrands />
       {/* Your other content */}
     </div>
   );
