e2c7e257d30b41f0f596933c824cc421c53b72e4
thilankadilshan <thilanka.cv@gmail.com>
2025-04-25 23:45:45 +0530

    nav updated Changes to be committed: 	modified:   client/src/App.jsx 	modified:   client/src/components/Header/Header.css 	modified:   client/src/components/Header/Header.jsx 	modified:   client/src/components/NavBar/NavBar.css 	modified:   client/src/components/NavBar/NavBar.jsx 	modified:   client/src/components/SearchBar/SearchBar.css 	modified:   client/src/main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
+// App.jsx
 import React from "react";
-import { BrowserRouter } from "react-router-dom"; // 👈 import this
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      {/* 👈 wrap your routes in this */}
+    <>
       <AppRoutes />
-    </BrowserRouter>
+    </>
   );
 };
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Make sure to import Link
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
 import SearchBar from "../SearchBar/SearchBar";
 import NavBar from "../NavBar/NavBar";
+import { FaSearch, FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const [isSearchOpen, setSearchOpen] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isAnimatingClose, setAnimatingClose] = useState(false);
+  const location = useLocation();
+
+  const toggleSearch = () => setSearchOpen(!isSearchOpen);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(true);
+    setAnimatingClose(false);
+  };
+
+  const closeSidebar = () => {
+    setAnimatingClose(true);
+    setTimeout(() => {
+      setSidebarOpen(false);
+      setAnimatingClose(false);
+    }, 300); // Matches CSS animation duration
+  };
+
   return (
     <header className="header">
       <div className="branding">
-        {/* Wrap the logo image with Link */}
         <Link to="/home">
           <img src={Logo} alt="TractorPartsLk Home" className="logo" />
         </Link>
-        {/* Wrap the title text with Link. Added inline style to prevent default link styling */}
         <Link to="/home" style={{ textDecoration: "none", color: "inherit" }}>
           <h1 className="title">TractorPartsLk</h1>
         </Link>
       </div>
-      <SearchBar />
-      <NavBar />
+
+      <div className="desktop-search">
+        <SearchBar />
+      </div>
+
+      <div className="nav-actions">
+        <button className="icon-btn mobile-only" onClick={toggleSearch}>
+          <FaSearch />
+        </button>
+        <button className="icon-btn mobile-only" onClick={toggleSidebar}>
+          <FaBars />
+        </button>
+      </div>
+
+      <NavBar currentPath={location.pathname} />
+
+      {isSearchOpen && (
+        <div className="popup-search mobile-only">
+          <SearchBar />
+        </div>
+      )}
+
+      {(isSidebarOpen || isAnimatingClose) && (
+        <div className={`sidebar ${isAnimatingClose ? "slide-out" : ""}`}>
+          <button className="close-btn" onClick={closeSidebar}>
+            ×
+          </button>
+          <NavBar currentPath={location.pathname} isMobile />
+        </div>
+      )}
     </header>
   );
 };
