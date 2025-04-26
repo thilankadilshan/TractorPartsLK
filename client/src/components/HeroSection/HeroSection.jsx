7e4a2f4069509a5f9c693753cf881406e40b47d2
thilankadilshan <thilanka.cv@gmail.com>
2025-04-26 18:37:45 +0530

    Changes to be committed: 	new file:   client/src/assets/hero1-m.png 	new file:   client/src/assets/hero2-m.png 	new file:   client/src/assets/hero3-m.png 	new file:   client/src/assets/hero4-m.png 	new file:   client/src/assets/hero5-m.png 	modified:   client/src/components/HeroSection/HeroSection.css 	modified:   client/src/components/HeroSection/HeroSection.jsx

--- client/src/components/HeroSection/HeroSection.jsx
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -7,14 +7,26 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./HeroSection.css";
 
-// Import images from src/assets
-import banner1 from "../../assets/hero1.png";
-import banner2 from "../../assets/hero2.png";
-import banner3 from "../../assets/hero3.png";
-import banner4 from "../../assets/hero4.png";
-import banner5 from "../../assets/hero5.png";
+// Import images for desktop and mobile
+import banner1Desktop from "../../assets/hero1.png";
+import banner2Desktop from "../../assets/hero2.png";
+import banner3Desktop from "../../assets/hero3.png";
+import banner4Desktop from "../../assets/hero4.png";
+import banner5Desktop from "../../assets/hero5.png";
 
-const banners = [banner1, banner2, banner3, banner4, banner5];
+import banner1Mobile from "../../assets/hero1-m.png";
+import banner2Mobile from "../../assets/hero2-m.png";
+import banner3Mobile from "../../assets/hero3-m.png";
+import banner4Mobile from "../../assets/hero4-m.png";
+import banner5Mobile from "../../assets/hero5-m.png";
+
+const banners = [
+  { desktop: banner1Desktop, mobile: banner1Mobile },
+  { desktop: banner2Desktop, mobile: banner2Mobile },
+  { desktop: banner3Desktop, mobile: banner3Mobile },
+  { desktop: banner4Desktop, mobile: banner4Mobile },
+  { desktop: banner5Desktop, mobile: banner5Mobile },
+];
 
 const HeroSection = () => {
   return (
@@ -27,12 +39,20 @@ const HeroSection = () => {
         loop
         className="hero-swiper"
       >
-        {banners.map((src, index) => (
+        {banners.map((banner, index) => (
           <SwiperSlide key={index}>
+            {/* Desktop Image */}
             <img
-              src={src}
-              alt={`Banner ${index + 1}`}
-              className="hero-image"
+              src={banner.desktop}
+              alt={`Desktop Banner ${index + 1}`}
+              className="hero-image desktop-banner"
+              loading="lazy"
+            />
+            {/* Mobile Image */}
+            <img
+              src={banner.mobile}
+              alt={`Mobile Banner ${index + 1}`}
+              className="hero-image mobile-banner"
               loading="lazy"
             />
           </SwiperSlide>
