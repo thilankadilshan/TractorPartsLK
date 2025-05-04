d9b321a7c8b2e6464eaa37dccbc7847774603264
thilankadilshan <thilanka.cv@gmail.com>
2025-05-04 05:35:43 +0530

    fixed errors Changes to be committed: 	modified:   client/src/components/Chatbot/Chatbot.jsx

--- client/src/components/Chatbot/Chatbot.jsx
+++ client/src/components/Chatbot/Chatbot.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./ChatBot.css"; // style separately
+import "./Chatbot.css"; // style separately
 
 const ChatBot = () => {
   const [messages, setMessages] = useState([
