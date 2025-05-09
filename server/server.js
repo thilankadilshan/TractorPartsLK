5228dcbad91071ead5fc537a29f793c672822b4b
thilankadilshan <thilanka.cv@gmail.com>
2025-05-10 01:30:09 +0530

    mongodb connected Changes to be committed: 	new file:   server/config/db.js 	deleted:    server/config/dn.js 	modified:   server/package-lock.json 	modified:   server/package.json 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
-const app = require('./App');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const app = require('./App'); // IMPORT the app
+
+dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
